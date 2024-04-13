a5ff797fb84e2233e1a152b341f346f53525608f
Doiiars <doiiars@qq.com>
2024-04-13 13:42:51 +0800

    规范代码
    规范代码

--- main.py
+++ main.py
@@ -42,74 +42,74 @@ def convert_json_to_custom_yaml(json_file_path, dirname):
     返回:
     - yaml_contents: 字符串，转换后的YAML内容。
     """
-        with open(json_file_path, 'r', encoding='utf-8') as json_file:
-            json_data = json.load(json_file)
+    with open(json_file_path, 'r', encoding='utf-8') as json_file:
+        json_data = json.load(json_file)
 
-        yaml_contents = ""
+    yaml_contents = ""
 
-        if isinstance(json_data, list):
-            for item in json_data:
-                # 处理字典类型的name字段
-                name = item.get("name", "")
-                if not name:
-                    continue
-                type = item.get("type", "")
-                if type != 'MOD_INFO':
-                    continue
-                if isinstance(name, dict):
-                    name = " | ".join([f"{k}: {v}" for k, v in name.items()])
+    if isinstance(json_data, list):
+        for item in json_data:
+            # 处理字典类型的name字段
+            name = item.get("name", "")
+            if not name:
+                continue
+            type = item.get("type", "")
+            if type != 'MOD_INFO':
+                continue
+            if isinstance(name, dict):
+                name = " | ".join([f"{k}: {v}" for k, v in name.items()])
 
-                id_value = item.get("id", item.get("ident", "unknown"))
-                if isinstance(id_value, str):
-                    id_value = f'{id_value}'
-                elif isinstance(id_value, list):
-                    id_value = ''.join([f'    - {id}\n' for id in id_value])
-                    
-                authors = item.get("authors", "unknown")
-                if isinstance(authors, str):
-                    authors = [f'{authors}']
-                    authors = ''.join([f'    - "{author}"\n' for author in authors])
-                elif isinstance(authors, list):
-                    authors = ''.join([f'    - "{author}"\n' for author in authors])
-                    
-                maintainers = item.get("maintainers", "unknown")
-                if isinstance(maintainers, str):
-                    maintainers = [f'{maintainers}']
-                    maintainers = ''.join([f'    - {maintainer}\n' for maintainer in maintainers])
-                elif isinstance(maintainers, list):
-                    maintainers = ''.join([f'    - {maintainer}\n' for maintainer in maintainers])
+            id_value = item.get("id", item.get("ident", "unknown"))
+            if isinstance(id_value, str):
+                id_value = f'{id_value}'
+            elif isinstance(id_value, list):
+                id_value = ''.join([f'    - {id}\n' for id in id_value])
                 
-                # 处理多行description字段
-                description = item.get("description", "No description provided.")
-                formatted_description = ""
-                if isinstance(description, str):
-                    formatted_description = "\n    ".join(description.splitlines())
-                elif isinstance(description, list):
-                    formatted_description = "\n    ".join(description)
-                elif isinstance(description, dict):
-                    formatted_description = "\n    ".join([f"{k}: {v}" for k, v in description.items()])
-
-                # 构造homepage字段的URL
-                download_url = f"https://alist.doiiars.com/d/Public/Cataclysmdda/{dirname}.zip"
+            authors = item.get("authors", "unknown")
+            if isinstance(authors, str):
+                authors = [f'{authors}']
+                authors = ''.join([f'    - "{author}"\n' for author in authors])
+            elif isinstance(authors, list):
+                authors = ''.join([f'    - "{author}"\n' for author in authors])
                 
-                folder_size = calculate_folder_size(dirname)
-                
-                yaml_contents = "- type: direct_download\n" 
-                yaml_contents += '  ident: "' + id_value + '"\n' 
-                yaml_contents += '  name: "' + name + '"\n' 
-                yaml_contents += "  authors: \n" + authors
-                yaml_contents += '  maintainers: \n' + maintainers
-                yaml_contents += "  description: |\n    " + formatted_description + "\n" 
-                yaml_contents += "  category: '" + item.get("category", "unknown") + "'\n" 
-                yaml_contents += "  dependencies:\n" + ''.join([f"    - '{dependency}'\n" for dependency in item.get("dependencies", [])]) 
-                yaml_contents += f"  size: {folder_size}\n" 
-                yaml_contents += '  url: "' + download_url + '"\n' 
-                yaml_contents += "  homepage: 'https://github.com/Kenan2000/CDDA-Structured-Kenan-Modpack'"
+            maintainers = item.get("maintainers", "unknown")
+            if isinstance(maintainers, str):
+                maintainers = [f'{maintainers}']
+                maintainers = ''.join([f'    - {maintainer}\n' for maintainer in maintainers])
+            elif isinstance(maintainers, list):
+                maintainers = ''.join([f'    - {maintainer}\n' for maintainer in maintainers])
+            
+            # 处理多行description字段
+            description = item.get("description", "No description provided.")
+            formatted_description = ""
+            if isinstance(description, str):
+                formatted_description = "\n    ".join(description.splitlines())
+            elif isinstance(description, list):
+                formatted_description = "\n    ".join(description)
+            elif isinstance(description, dict):
+                formatted_description = "\n    ".join([f"{k}: {v}" for k, v in description.items()])
 
-            return yaml_contents
-        else:
-            print(f"文件 {json_file_path} 的格式不正确。期望的是一个列表。")
-            return ""
+            # 构造homepage字段的URL
+            download_url = f"https://alist.doiiars.com/d/Public/Cataclysmdda/{dirname}.zip"
+            
+            folder_size = calculate_folder_size(dirname)
+            
+            yaml_contents = "- type: direct_download\n" 
+            yaml_contents += '  ident: "' + id_value + '"\n' 
+            yaml_contents += '  name: "' + name + '"\n' 
+            yaml_contents += "  authors: \n" + authors
+            yaml_contents += '  maintainers: \n' + maintainers
+            yaml_contents += "  description: |\n    " + formatted_description + "\n" 
+            yaml_contents += "  category: '" + item.get("category", "unknown") + "'\n" 
+            yaml_contents += "  dependencies:\n" + ''.join([f"    - '{dependency}'\n" for dependency in item.get("dependencies", [])]) 
+            yaml_contents += f"  size: {folder_size}\n" 
+            yaml_contents += '  url: "' + download_url + '"\n' 
+            yaml_contents += "  homepage: 'https://github.com/Kenan2000/CDDA-Structured-Kenan-Modpack'"
+
+        return yaml_contents
+    else:
+        print(f"文件 {json_file_path} 的格式不正确。期望的是一个列表。")
+        return ""
 
 
 
